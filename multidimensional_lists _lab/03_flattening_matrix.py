43082401679b8535ad14b61a2caf45afe66f729c
Mario <144950133+Mario8802@users.noreply.github.com>
2023-12-30 23:35:56 +0100

    Add files via upload

---
+++ multidimensional_lists _lab/03_flattening_matrix.py
@@ -0,0 +1,17 @@
+# matrix = [["M" for j in range(2)]for i in range(3)]
+# print(matrix)
+
+rows = int(input())
+matrix = []
+
+for row in range(rows):
+    elements = [int(el) for el in input().split(',')]
+    matrix.append(elements)
+
+flattened = []
+
+for row_index in range(rows):
+    for col_index in range(len(matrix[row_index])):
+        flattened.append(matrix[row_index][col_index])
+
+print(flattened)
