6e3d190b868c66c7e1102918d00929270cc878dd
Mario <144950133+Mario8802@users.noreply.github.com>
2023-12-31 21:46:29 +0100

    Add files via upload

---
+++ multidimensional_lists _lab/04_sum_matrix_columns.py
@@ -0,0 +1,18 @@
+# matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# [print(num) for num in [j for j in matrix]]
+
+data = input().split(', ')
+rows = int(data[0])
+cols = int(data[1])
+
+matrix = []
+
+for row in range(rows):
+    elements = [int(el) for el in input().split()]
+    matrix.append(elements)
+
+for col_index in range(cols):
+    sum_col_nums = 0
+    for row_index in range(rows):
+        sum_col_nums += matrix[row_index][col_index]
+    print(sum_col_nums)
