fe3bf39bb5c5ae32929d5c08c587c52797b5c92e
Mario <144950133+Mario8802@users.noreply.github.com>
2024-01-15 21:13:24 +0100

    Update 04_count_symbols.py

--- tuples_and_sets_exercise/04_count_symbols.py
+++ tuples_and_sets_exercise/04_count_symbols.py
@@ -3,4 +3,16 @@ unique_symbols = sorted(set(text))
 
 for char in unique_symbols:
     print(f"{char}: {text.count(char)} time/s")
+-------------------------------------------------------
+text = input()
 
+for symbol in sorted(set(text)):
+    print(f"{symbol}: {text.count(symbol)} time/s")
+-------------------------------------------------------
+# occurrences = {}
+#
+# for symbol in input():
+#     occurrences[symbol] = occurrences.get(symbol, 0) + 1
+#
+# for symbol, times in sorted(occurrences.items()):  # (("a", 1), ("b", 3))
+#     print(f"{symbol}: {times} time/s")
