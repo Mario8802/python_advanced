31b21abefbe0e2f307f4b0f609fc24e25d242f7a
Mario <144950133+Mario8802@users.noreply.github.com>
2024-01-15 21:15:51 +0100

    Update 01_unique_usernames.py

--- tuples_and_sets_exercise/01_unique_usernames.py
+++ tuples_and_sets_exercise/01_unique_usernames.py
@@ -6,3 +6,21 @@ for _ in range(n):
     unique_usernames.add(input())
 
 print(*unique_usernames, sep="\n")
+
+
+--------------------------------------------------------
+
+print(*{input() for _ in range(int(input()))}, sep="\n")
+
+
+---------------------------------------------------------
+
+
+# solution 2
+# names_count = int(input())
+# names = set()
+#
+# for _ in range(names_count):
+#     names.add(input())
+#
+# print(*names, sep="\n")
