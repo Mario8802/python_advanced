d9347ce25289912beb1ae01640d26f93d772fc68
Mario <144950133+Mario8802@users.noreply.github.com>
2024-01-15 21:14:35 +0100

    Update 03_periodic_table.py

--- tuples_and_sets_exercise/03_periodic_table.py
+++ tuples_and_sets_exercise/03_periodic_table.py
@@ -7,3 +7,20 @@ for _ in range(n):
     for el in elements:
         unique_elements.add(el)
 print(*unique_elements, sep='\n')
+
+
+------------------------------------------------------
+table = set()
+
+for _ in range(int(input())):
+    for el in input().split():  # input().split() -> ["Ce", "O", "H"]
+        table.add(el)
+
+print(*table, sep="\n")
+
+---------------------------------------------------------
+
+
+
+# print(*{el for _ in range(int(input())) for el in input().split()}, sep="\n")
+# not a good idea
