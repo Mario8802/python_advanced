6084f5b9e95c02e6f3eff6d65c64d67dc9490757
Mario <144950133+Mario8802@users.noreply.github.com>
2024-01-22 22:49:13 +0100

    Update 01_flatten_lists.py

--- multidimensional_lists_exercise_2/01_flatten_lists.py
+++ multidimensional_lists_exercise_2/01_flatten_lists.py
@@ -9,3 +9,21 @@ for i in range(len(strings) - 1, -1, - 1):
         matrix.append(row)
 for row in matrix:
     print(*row, sep=' ', end=' ')
+
+
+
+----------------------------------------------------------------------------
+
+line = input().split("|")  # ["7 88", "4    5 6", "1 2   3"]
+
+sub_lists = []  # [1, 2, 3].extend([4, 5, 6]) -> [1, 2, 3, 4, 5, 6]
+
+for sub_string in line[::-1]:
+    sub_lists.extend(sub_string.split(" "))  #  "4    5 6" -> [4, 5, 6]
+
+print(*sub_lists)
+
+
+# solution 2
+numbers = [string.split() for string in input().split("|")]  # [[7, 88], [4, 5, 6], ...] -> 123|    |456
+print(*[' '.join(sub_list) for sub_list in numbers[::-1] if sub_lists])  # ["1 2 3", "4 5 6", "7 88"]
