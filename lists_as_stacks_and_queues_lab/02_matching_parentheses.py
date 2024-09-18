684f3d071a8a9ff10cceb49e1a65eaa232fd2d60
Mario <144950133+Mario8802@users.noreply.github.com>
2024-09-18 23:56:53 +0200

    Update 02_matching_parentheses.py
     adding an if statement Ensures there's something to pop

--- lists_as_stacks_and_queues_lab/02_matching_parentheses.py
+++ lists_as_stacks_and_queues_lab/02_matching_parentheses.py
@@ -1,10 +1,23 @@
-expression = input()
+# expression = input()
+# stack = []
+
+# for index in range(len(expression)):
+#     if expression[index] == "(":
+#         stack.append(index)
+#     elif expression[index] == ")":
+#         start_index = stack.pop()
+#         last_index = index + 1
+#         print(expression[start_index:last_index])
+
+
+algebraic_expression = input()
+
 stack = []
 
-for index in range(len(expression)):
-    if expression[index] == "(":
-        stack.append(index)
-    elif expression[index] == ")":
-        start_index = stack.pop()
-        last_index = index + 1
-        print(expression[start_index:last_index])
+for i, v in enumerate(algebraic_expression):
+    if v == "(":
+        stack.append(i)
+    elif v == ")":
+        if stack:  
+            opening_bracket = stack.pop()
+            print(algebraic_expression[opening_bracket:i + 1])
