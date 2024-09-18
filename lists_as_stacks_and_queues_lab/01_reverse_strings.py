ce9fa4ca1460e7984e9be0900ed9ca4ab0d37d58
Mario <144950133+Mario8802@users.noreply.github.com>
2024-09-18 23:55:21 +0200

    Update 01_reverse_strings.py

--- lists_as_stacks_and_queues_lab/01_reverse_strings.py
+++ lists_as_stacks_and_queues_lab/01_reverse_strings.py
@@ -1,4 +1,15 @@
-some_text = list(input())
+# some_text = list(input())
 
-while some_text:
-    print(some_text.pop(),end="")
+# while some_text:
+#     print(some_text.pop(),end="")
+
+
+some_string = input()
+
+my_stack = []
+
+for i in some_string:
+    my_stack.append(i)
+
+while my_stack:
+    print(my_stack.pop(), end='')
